8a205bdebbda6b6738cf2c9b51290e5d01dbaec7
Joel Jaison <joeljaison394@gmail.com>
2023-06-11 08:42:12 +0530

    authentication updtaed and verfied , working properly

--- HealthBooker-main/client/src/pages/Register.jsx
+++ HealthBooker-main/client/src/pages/Register.jsx
@@ -4,10 +4,7 @@ import "../styles/register.css";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
-
 function Register() {
-  const [file, setFile] = useState("");
   const [loading, setLoading] = useState(false);
   const [formDetails, setFormDetails] = useState({
     firstname: "",
@@ -20,80 +17,85 @@ function Register() {
 
   const inputChange = (e) => {
     const { name, value } = e.target;
-    return setFormDetails({
+    setFormDetails({
       ...formDetails,
       [name]: value,
     });
   };
 
-  const onUpload = async (element) => {
-    setLoading(true);
-    if (element.type === "image/jpeg" || element.type === "image/png") {
-      const data = new FormData();
-      data.append("file", element);
-      data.append("upload_preset", "zenstore");
-      data.append("cloud_name", process.env.REACT_APP_CLOUDINARY_CLOUD_NAME);
-      fetch(process.env.REACT_APP_CLOUDINARY_BASE_URL, {
-        method: "POST",
-        body: data,
-      })
-        .then((res) => res.json())
-        .then((data) => setFile(data.url.toString()));
-      setLoading(false);
-    } else {
-      setLoading(false);
-      toast.error("Please select an image in jpeg or png format");
-    }
-  };
-
   const formSubmit = async (e) => {
     try {
       e.preventDefault();
-
-      if (loading) return;
-      if (file === "") return;
-
-      const { firstname, lastname, email, password, confpassword } =
-        formDetails;
-      if (!firstname || !lastname || !email || !password || !confpassword) {
-        return toast.error("Input field should not be empty");
-      } else if (firstname.length < 3) {
-        return toast.error("First name must be at least 3 characters long");
-      } else if (lastname.length < 3) {
-        return toast.error("Last name must be at least 3 characters long");
-      } else if (password.length < 5) {
-        return toast.error("Password must be at least 5 characters long");
-      } else if (password !== confpassword) {
-        return toast.error("Passwords do not match");
+  
+      // Validate form fields
+      if (!validateForm()) {
+        return;
       }
+  
+      setLoading(true);
+  
+      const { firstname, lastname, email, password } = formDetails;
+      console.log(formDetails);
+  
+      // Send registration request
+      const response = await axios.post("http://localhost:5000/api/user/register", {
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        password: password,
+      });
+  
+      // Display toast based on the response
+      if (response.status === 201) {
+        toast.success("User registered successfully");
+        navigate("/login");
+      } else {
+        toast.error("Unable to register user");
+      }
+    } catch (error) {
+      toast.error("An error occurred during registration");
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+  const validateForm = () => {
+    const { firstname, lastname, email, password, confpassword } = formDetails;
 
-      const { data } = await toast.promise(
-        axios.post("/user/register", {
-          firstname,
-          lastname,
-          email,
-          password,
-          pic: file,
-        }),
-        {
-          pending: "Registering user...",
-          success: "User registered successfully",
-          error: "Unable to register user",
-          loading: "Registering user...",
-        }
-      );
-      return navigate("/login");
-    } catch (error) {}
+    if (!firstname || !lastname || !email || !password || !confpassword) {
+      toast.error("Input field should not be empty");
+      return false;
+    }
+
+    if (firstname.length < 3) {
+      toast.error("First name must be at least 3 characters long");
+      return false;
+    }
+
+    if (lastname.length < 3) {
+      toast.error("Last name must be at least 3 characters long");
+      return false;
+    }
+
+    if (password.length < 5) {
+      toast.error("Password must be at least 5 characters long");
+      return false;
+    }
+
+    if (password !== confpassword) {
+      toast.error("Passwords do not match");
+      return false;
+    }
+
+    return true;
   };
 
   return (
     <section className="register-section flex-center">
       <div className="register-container flex-center">
         <h2 className="form-heading">Sign Up</h2>
-        <form
-          onSubmit={formSubmit}
-          className="register-form"
-        >
+        <form onSubmit={formSubmit} className="register-form">
           <input
             type="text"
             name="firstname"
@@ -119,13 +121,6 @@ function Register() {
             onChange={inputChange}
           />
           <input
-            type="file"
-            onChange={(e) => onUpload(e.target.files[0])}
-            name="profile-pic"
-            id="profile-pic"
-            className="form-input"
-          />
-          <input
             type="password"
             name="password"
             className="form-input"
@@ -144,17 +139,14 @@ function Register() {
           <button
             type="submit"
             className="btn form-btn"
-            disabled={loading ? true : false}
+            disabled={loading}
           >
-            sign up
+            {loading ? "Registering..." : "Sign up"}
           </button>
         </form>
         <p>
           Already a user?{" "}
-          <NavLink
-            className="login-link"
-            to={"/login"}
-          >
+          <NavLink className="login-link" to={"/login"}>
             Log in
           </NavLink>
         </p>
